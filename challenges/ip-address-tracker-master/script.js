86aef57c949019ccbd5d446e574c19b6468f80bc
joseito.terence <joseito.terence@gmail.com>
2022-01-16 21:26:51 +0530

    Refresh ui after fetch

--- challenges/ip-address-tracker-master/script.js
+++ challenges/ip-address-tracker-master/script.js
@@ -1,4 +1,13 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+(async function main() {
+  const form = document.forms['form'];
+  form.onsubmit = handleSubmit;
+
+  await initialize();
+
+})();
+
+
+var map = L.map('map').setView([37.38605, -122.08385], 13);
 
 const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   maxZoom: 18,
@@ -8,3 +17,41 @@ const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
   tileSize: 512,
   zoomOffset: -1
 }).addTo(map);
+
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const ipAddress = form.ip.value;
+
+  const res = await ipify(ipAddress);
+  refreshUI(res);
+}
+
+async function initialize() {
+  const res = await ipify('8.8.8.8');
+  refreshUI(res);
+}
+
+function refreshUI({ location, isp, ip }) {
+  const { timezone, lat, lng, region, country, postalCode } = location;
+  positionMarker(lat, lng);
+
+  $('#ip').innerText = ip;
+  $('#isp').innerText = isp;
+  $('#timezone').innerText = `UTC ${timezone}`;
+  $('#location').innerText = `${region}, ${country} ${postalCode}`;
+}
+
+async function ipify(ipAddress) {
+  const res = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_6L6NdauxsZ2DOZhAoqSEP4i9Uar19&ipAddress=${ipAddress}`);
+  return await res.json();
+}
+
+function positionMarker(lat, lng) {
+  const icon = L.icon({ iconUrl: './images/icon-location.svg' });
+
+  const marker = L.marker([lat, lng], { icon }).addTo(map);
+}
+
+const $ = (selector) => document.querySelector(selector);
+
