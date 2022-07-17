3bfbcf4de5dd18059bd5bc62470f68dac294616f
Joseito Fernandes <joseito.terence@gmail.com>
2022-07-17 20:13:14 +0530

    added: next progress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import NextProgress from "next-progress"
 
 const theme: DefaultTheme = {
   colors: {
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <NextProgress delay={300} options={{ showSpinner: false }} />
         <Component {...pageProps} />
       </ThemeProvider>
     </QueryClientProvider>
