279c154ee002c1d7d7722d2f90074e4174b8165f
Joseito Fernandes <joseito.terence@gmail.com>
2023-07-09 13:45:43 +0530

    job listing challenge

--- types.ts
+++ types.ts
@@ -13,3 +13,19 @@ export type MovieType = {
   duration: string,
   year: number
 }
+
+export type JobType = {
+  id: number,
+  company: string,
+  logo: string,
+  new: boolean,
+  featured: boolean,
+  position: string,
+  role: string,
+  level: string,
+  postedAt: string,
+  contract: string,
+  location: string,
+  languages: string[],
+  tools: string[]
+}
